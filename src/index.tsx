9438350c6e07ea7421abf52271f9053b047bfae3
Xavier <xavier.ladet@yahoo.fr>
2022-07-30 22:33:14 +0200

    Update from render to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './components/App/index';
 
 
 const rootReactElement = <App />;
 const target = document.getElementById('root');
-render(rootReactElement, target);
+const root = createRoot(target);
+root.render(rootReactElement);
